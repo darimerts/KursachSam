3ea60291030436f95b7f933e10954e59203c6fb7
darimerts <darimerts@gmail.com>
2015-05-28 17:53:05 +0300

    kjkj

--- qwerty.m
+++ qwerty.m
@@ -8,7 +8,8 @@ A = fscanf(fileID,formatSpec);
 fclose(fileID);
 
 %Задание времени и амплитуды
-i = 1:length(A);
+N = length(A);
+i = 1:N;
 Fd = 200;
 Time = i/Fd;
 U = A/(10^3);
@@ -18,19 +19,111 @@ R = U/I;
 
 
 %Вывод графика
+%figure;
+%plot(Time,R);
+
+
+Fy=fft(R);
+F1=50/60; F2=90/60;
+Tov=N/Fd;
+dF=1/Tov;
+F(i)=i*dF;
+j=1:1:N/2;
+%figure;
+%plot(F(j),abs(Fy(j)));
+
+for i=1:1:(N+1)/2
+    if F(i)<F1 
+        Fy(i)=0;
+        Fy(N-i)=0;
+    end;
+    if F(i)>F2 
+        Fy(i)=0;
+        Fy(N-i)=0;
+    end;
+    if F(i)>(F2-dF)
+        if F(i)<(F2+dF)
+            imax=i;
+        end;
+    end;
+end;
+
+j=1:1:imax;
+y=real(ifft(Fy));
+
+%figure;
+%plot(Time,y);
+
+i=1:1:N-1;
+dy(i)=y(i+1)-y(i);
+
+max=y(1);
+temp=0; N1=0; xmax=0; ymax=0; Sum=0; s=0;
+for i=1:1:N
+    Sum=Sum+abs(y(i));
+    if y(i)>max
+        max=y(i);
+        xmax=i;
+        temp=0;
+    else
+        if temp==0
+            if xmax>0
+                if y(i)<max*0.5
+                    N1=N1+1;
+                    imax(N1)=xmax;
+                    ym(N1)=max;
+                    xm(N1)=Time(imax(N1));
+                    yy(N1)=R(fix(xmax));
+                    max=0.5*max;
+                    temp=1;
+                end
+            end
+            
+        end
+    end
+end 
+
+min=y(1);
+temp=0; N2=0; xmin=0; ymin=0; Sum=0; s=0;
+for i=1:1:N
+    Sum=Sum+abs(y(i));
+    if y(i)<min
+        min=y(i);
+        xmin=i;
+        temp=0;
+    else
+        if temp==0
+            if xmin>0
+                if y(i)>min+50;
+                    N2=N2+1;
+                    imin(N2)=xmin;
+                    ymi(N2)=min;
+                    xmi(N2)=Time(imin(N2));
+                    yyy(N2)=R(fix(xmin));
+                    min=min+50;
+                    temp=1;
+                end
+            end
+            
+        end
+    end
+end 
+
+k=0;n=0;
+for i=1:N1
+    if k<=N2
+        if imin(k)<imax(i)
+            cycleb(n) = fminbnd('R',imax(i),imin(k));
+            n=n+1;
+            k=k+1;
+        else
+            k=k+1;
+        end
+    end
+end
+
+
+
 figure;
-plot(Time,R);
-
-%производная
-Udif = diff(R)*Fd;
-Udif(length(A)) = 0;
-
-%Вывод графика
-figure;
-plot(Time,Udif);
-
-%Поиск амплитуды инцизуры
-
-%for i=(1:1:(length(A)-1)
-   % if A
+plot(Time,R,xm,yy,'ko',xmi,yyy,'ko');
 
