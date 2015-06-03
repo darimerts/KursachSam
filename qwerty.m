d4bd356faff851ecd8614a53839ad2c58cf57888
darimerts <darimerts@gmail.com>
2015-06-03 23:35:41 +0300

    сюсю

--- qwerty.m
+++ qwerty.m
@@ -7,7 +7,7 @@ formatSpec = '%d';
 A = fscanf(fileID,formatSpec);
 fclose(fileID);
 
-%Задание времени и амплитуды
+%Задание времени и амплитуды диф.регоэнцефалограммы
 N = length(A);
 i = 1:N;
 Fd = 200;
@@ -15,39 +15,97 @@ Time = i/Fd;
 U = A/(10^3);
 I = 1/(10^3);
 R = U/I;
+d=length(R);
+%Вычисление реограммы
+sym(1)=0;
+for i=(2:1:N)
+  sym(i)=R(i-1)+sym(i-1); 
+end;
+Rint=sym;
 
+%Вывод графика полученной РЭГ
+figure;
+plot(Time,Rint);
 
-
-%Вывод графика
-%figure;
-%plot(Time,R);
-FF=fft(R);
-F1=0.3; F2=30;
+FF1=fft(Rint);
+%F1=50; F2=30;
+Flow=0;
 Tov=N/Fd;
 dF=1/Tov;
 F(i)=i*dF;
 j=1:1:N/2;
+%figure;
+%plot(abs(FF1(j)),F(i));
+
+
+
+%Расчет и построение спектра РЭГ
+N1=length(Rint);
+Tov=N1/Fd;
+F1=50;
+dF=1/Tov;
+F(i)=i*dF;
+j=1:1:N1;
+SpectrRint=fft(Rint);
+
+figure;
+plot(abs(SpectrRint(j)));
+figure;
+plot(Time,real(ifft(SpectrRint)));
+
+for i=1:1:(N1+1)/2
+    if i<50
+        SpectrRint(i)=0;
+        SpectrRint(N1-i+1)=0;
+    end;
+       % if i>10000 
+        %SpectrRint(i)=0;
+        %SpectrRint(N1-i+1)=0;
+    %end;
+    end;
+    
+Rintn=real(ifft(SpectrRint));
+
+figure;
+plot(Time,Rintn);
+
+figure;
+plot(abs(fft(Rintn)));
+
+fname =['C:\Users\Дарина\Documents\GitHub\KursachSam\111.mat']; 
+load(fname);
+legh_fi=length(G)-1;
+x=zeros([1,length(Rint)]);
+y=zeros([1,length(Rint)]);
+x=Rint;
+%figure;
+%plot(Time,x);
+for k=1:legh_fi
+for i=3:length(y)
+y(i)=((SOS(k,4)*x(i)+SOS(k,5)*x(i-1)+SOS(k,6)*x(i-2))*(G(k))-(SOS(k,2)*y(i-1)+SOS(k,3)*y(i-2)))/SOS(k,1);
+end;
+for i=3:length(y)
+x(i)=y(i);
+end;
+end;
+Rintf=y;
+%Rint=real(ifft(x));
+
+
+FF10=fft(Rint,100);
+b=length(FF);
+%figure;
+%plot(F,abs(FF10));
 for i=1:1:(N+1)/2
     if F(i)<F1 
         FF(i)=0;
         FF(N-i)=0;
     end;
-    if F(i)>F2 
-        FF(i)=0;
-        FF(N-i)=0;
     end;
-    if F(i)>(F2-dF)
-        if F(i)<(F2+dF)
-            imax=i;
-        end;
-    end;
-end;
-R=real(ifft(FF));
+Rintn=real(ifft(FF));
 
-%figure;
-%plot(Time,R);
-
-
+figure;
+plot(Time,Rintn);
 
 Fy=fft(R);
 F1=50/60; F2=90/60;
@@ -156,5 +214,5 @@ i = 1:N;
 
 %figure;
 %plot(Time,R,xm,yy,'ko',xmi,yyy,'ko');
-figure;
-plot(i,R,period,RRR,'ko');
+%figure;
+%plot(i,R,period,RRR,'ko');
